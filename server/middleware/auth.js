8145d82b3bf11ff0e216e4f2bc11ce0eb9945440
ductoanthanh <toanth@metropolia.fi>
2018-08-18 14:06:01 +0300

    Update authentication middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ const { User } = require('./../models/user');
 let auth = (req,res,next) => {
     let token = req.cookies.w_auth;
 
-    User.findByToken(token,(err, user)=>{
+    User.findByToken(token,(err,user)=>{
         if(err) throw err;
         if(!user) return res.json({
             isAuth: false,
