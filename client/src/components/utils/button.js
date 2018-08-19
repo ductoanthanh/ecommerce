d32ab4735a1b69e230985fa47cd3052e7bce7760
ductoanthanh <arsenaltoan10@gmail.com>
2018-08-20 00:25:42 +0300

    Add bag_link case for button

--- client/src/components/utils/button.js
+++ client/src/components/utils/button.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faShoppingBag from '@fortawesome/fontawesome-free-solid/faShoppingBag';
+
 const MyButton = (props) => {
   const buttons = () => {
     let template = '';
-    
+
     switch(props.type) {
       case "default":
         template = <Link
-            className="link_default"
+            className= { props.altClass ? props.altClass : 'link_default' }
             to={props.linkTo}
             {...props.addStyles}
           >
           {props.title}
         </Link>
+      break;
+      case "bag_link":
+        template =
+          <div
+            className="bag_link"
+            onClick={() => {
+              props.runAction();
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faShoppingBag}
+            />
+          </div>
       break;
       default:
         template = '';
