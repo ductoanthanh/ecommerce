1bc80cc0f8770c3ed297b16dabfaf70babda3a51
ductoanthanh <arsenaltoan10@gmail.com>
2018-08-19 23:12:14 +0300

    Dispatch actions to get products sort by arrival & sold

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
 import HomeSlider from './home_slider';
+import HomePromotion from './home_promotion';
+
+import { connect } from 'react-redux';
+import { getProductsBySell, getProductsByArrival } from '../../actions/productActions';
 
 class Home extends Component {
+  componentDidMount() {
+    this.props.dispatch(getProductsBySell()).then(()=> {
+            console.log(this.props);
+    });
+    this.props.dispatch(getProductsByArrival());
+  }
+
   render() {
     return (
       <div>
         <HomeSlider />
+        <HomePromotion />
       </div>
     )
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    products: state.products
+  }
+}
+
+export default connect(mapStateToProps)(Home);
