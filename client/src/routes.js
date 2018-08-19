6cd2fb23b36b6962ad844c44629f51aa10e17d07
ductoanthanh <arsenaltoan10@gmail.com>
2018-08-19 13:13:01 +0300

    Create user dashboard skeleton

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,11 +4,13 @@ import Home from './components/Home';
 import Layout from './hoc/layout';
 import RegisterLogin from './components/Register_Login';
 import Register from './components/Register_Login/register';
+import UserDashboard from './components/User/';
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/user/dashboard" exact component={UserDashboard} />
         <Route path="/register" component={Register} />
         <Route path="/register_login" component={RegisterLogin} />
         <Route path="/" exact component={Home} />
