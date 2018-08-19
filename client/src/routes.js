d210010a99ee369c4577691eb2a12a98f7a178de
ductoanthanh <toanth@metropolia.fi>
2018-08-19 16:18:34 +0300

    Create HOC auth to set preventing routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Layout from './hoc/layout';
+import Auth from './hoc/auth';
+
 import RegisterLogin from './components/Register_Login';
 import Register from './components/Register_Login/register';
 import UserDashboard from './components/User/';
@@ -10,10 +12,10 @@ const Routes = () => {
   return (
     <Layout>
       <Switch>
-        <Route path="/user/dashboard" exact component={UserDashboard} />
-        <Route path="/register" component={Register} />
-        <Route path="/register_login" component={RegisterLogin} />
-        <Route path="/" exact component={Home} />
+        <Route path="/user/dashboard" exact component={Auth(UserDashboard, true)} />
+        <Route path="/register" component={Auth(Register, false)} />
+        <Route path="/register_login" component={Auth(RegisterLogin, false)} />
+        <Route path="/" exact component={Auth(Home, null)} />
       </Switch>
     </Layout>
   )
