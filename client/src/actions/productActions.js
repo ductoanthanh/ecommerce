f5556f9facddd22fab1f1bd26973aa489d27801f
ductoanthanh <toanth@metropolia.fi>
2018-08-20 18:19:49 +0300

    Add Shop component,  update actions & reducer

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -2,25 +2,49 @@ import axios from 'axios';
 import { PRODUCT_SERVER } from '../components/utils/misc';
 import {
   GET_PRODUCTS_BY_SELL,
-  GET_PRODUCTS_BY_ARRIVAL
+  GET_PRODUCTS_BY_ARRIVAL,
+  GET_BRANDS,
+  GET_WOODS
 } from './types';
 
 export function getProductsBySell() {
   const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
             .then(response => response.data);
 
-    return {
-        type: GET_PRODUCTS_BY_SELL,
-        payload: request
-    }
+  return {
+      type: GET_PRODUCTS_BY_SELL,
+      payload: request
+  }
 }
 
 export function getProductsByArrival() {
   const request = axios.get(`${PRODUCT_SERVER}/articles?createdAt&order=desc&limit=4`)
             .then(response => response.data);
 
-    return {
-        type: GET_PRODUCTS_BY_ARRIVAL,
-        payload: request
-    }
+  return {
+      type: GET_PRODUCTS_BY_ARRIVAL,
+      payload: request
+  }
+}
+
+//CATEGORIES
+
+export function getBrands() {
+  const request = axios.get(`${PRODUCT_SERVER}/brands`)
+            .then(response => response.data);
+
+  return {
+      type: GET_BRANDS,
+      payload: request
+  }
+}
+
+export function getWoods() {
+  const request = axios.get(`${PRODUCT_SERVER}/woods`)
+            .then(response => response.data);
+
+  return {
+      type: GET_WOODS,
+      payload: request
+  }
 }
