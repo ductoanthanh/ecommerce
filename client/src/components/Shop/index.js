58cfe3964756a4b550a8c5237a77946e8cbb02c3
ductoanthanh <arsenaltoan10@gmail.com>
2018-08-20 22:19:36 +0300

    Finish checkbox filter logic

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -6,17 +6,36 @@ import { getBrands, getWoods } from '../../actions/productActions';
 
 import CollapseCheckbox from '../utils/collapseCheckbox';
 
+import { frets } from '../utils/Form/fixed_categories';
+
 class Shop extends Component {
+  state = {
+    grid: '',
+    limit: 6,
+    skip: 0,
+    filters: {
+      brand: [],
+      frets: [],
+      wood: [],
+      price: []
+    }
+  }
+
   componentDidMount() {
     this.props.dispatch(getBrands());
     this.props.dispatch(getWoods());
   }
 
-  handleFilters = () => {
-
+  handleFilters = (filters, category) => {
+    const newFilters = {...this.state.filters};
+    newFilters[category] = filters;
+    this.setState({
+      filters: newFilters
+    })
   }
 
   render() {
+    console.log(this.state.filters);
     const products = this.props.products;
     return (
       <div>
@@ -31,6 +50,18 @@ class Shop extends Component {
                 title="Brands"
                 list={products.brands}
                 handleFilters={(filters) => this.handleFilters(filters, 'brand')}
+              />
+              <CollapseCheckbox
+                initState={false}
+                title="Frets"
+                list={frets}
+                handleFilters={(filters) => this.handleFilters(filters, 'frets')}
+              />
+              <CollapseCheckbox
+                initState={true}
+                title="Wood"
+                list={products.woods}
+                handleFilters={(filters) => this.handleFilters(filters, 'wood')}
               />
             </div>
             <div className="right">
