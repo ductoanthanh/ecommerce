fb38c16e44f7e78fae3161d7da634ed5260b7dc9
ductoanthanh <toanth@metropolia.fi>
2018-08-21 10:00:44 +0300

    Add price filters

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -5,8 +5,9 @@ import PageTop from '../utils/page_top';
 import { getBrands, getWoods } from '../../actions/productActions';
 
 import CollapseCheckbox from '../utils/collapseCheckbox';
+import CollapseRadio from '../utils/collapseRadio';
 
-import { frets } from '../utils/Form/fixed_categories';
+import { frets, price } from '../utils/Form/fixed_categories';
 
 class Shop extends Component {
   state = {
@@ -26,9 +27,27 @@ class Shop extends Component {
     this.props.dispatch(getWoods());
   }
 
+  handlePrice = (value) => {
+    const data = price;
+    let array = [];
+
+    for(let key in data) {
+      if(data[key]._id === parseInt(value, 10)) {
+        array = data[key].array
+      }
+    }
+    return array;
+  }
+
   handleFilters = (filters, category) => {
     const newFilters = {...this.state.filters};
     newFilters[category] = filters;
+
+    if(category === "price") {
+      let priceValues = this.handlePrice(filters);
+      newFilters[category] = priceValues;
+    }
+
     this.setState({
       filters: newFilters
     })
@@ -62,6 +81,12 @@ class Shop extends Component {
                 title="Wood"
                 list={products.woods}
                 handleFilters={(filters) => this.handleFilters(filters, 'wood')}
+              />
+              <CollapseRadio
+                initState={true}
+                title="Price"
+                list={price}
+                handleFilters={(filters) => this.handleFilters(filters, 'price')}
               />
             </div>
             <div className="right">
