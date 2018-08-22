a1ad05e39c28a5c76486556d7061844cb8c156d8
ductoanthanh <arsenaltoan10@gmail.com>
2018-08-23 01:14:19 +0300

    Add new product

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -5,7 +5,8 @@ import {
   GET_PRODUCTS_BY_ARRIVAL,
   GET_BRANDS,
   GET_WOODS,
-  GET_PRODUCTS_TO_SHOP
+  GET_PRODUCTS_TO_SHOP,
+  ADD_PRODUCT
 } from './types';
 
 export function getProductsBySell() {
@@ -71,6 +72,15 @@ export function getProductsToShop(skip, limit, filters = [], previousState = [])
               });
   return {
     type: GET_PRODUCTS_TO_SHOP,
+    payload: request
+  }
+}
+
+export function addProduct(dataToSubmit) {
+  const request = axios.post(`${PRODUCT_SERVER}/article`, dataToSubmit)
+              .then(response => response.data);
+  return {
+    type: ADD_PRODUCT,
     payload: request
   }
 }
