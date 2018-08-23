c9bf37b4ca2d0208ef986115530963e063292278
ductoanthanh <toanth@metropolia.fi>
2018-08-24 00:48:49 +0300

    Finish adding woods

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -10,3 +10,4 @@ export const GET_PRODUCTS_TO_SHOP = 'GET_PRODUCTS_TO_SHOP';
 export const ADD_PRODUCT = 'ADD_PRODUCT';
 export const CLEAR_PRODUCT = 'CLEAR_PRODUCT';
 export const ADD_BRAND = 'ADD_BRAND';
+export const ADD_WOOD = 'ADD_WOOD';
