4c22e5aa22c2c6c53a770272d18c7bf1d1e8cec5
ductoanthanh <arsenaltoan10@gmail.com>
2018-08-25 11:56:22 +0300

    Setup paypal component

---
+++ client/src/components/utils/paypal.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PaypalExpressBtn from 'react-paypal-express-checkout';
+
+class Paypal extends Component {
+  render() {
+
+    const onSuccess = (payment) => {
+      console.log(JSON.stringify(payment));
+
+    }
+
+    const onCancel = (data) => {
+      console.log(JSON.stringify(data))
+    }
+
+    const onError = (err) => {
+      console.log(JSON.stringify(err))
+    }
+
+    let env = 'sandbox';
+    let currency = 'USD';
+    let total = this.props.toPay;
+
+    const client = {
+      sandbox:'AedpBcSWWoHVsEBc7htVluZsSwqQ-lyUfLHdDqXF39AB29eJhdS9TgL2xUy1lexw5fpuJe6n8oBja0fn',
+      production:''
+    }
+
+    return (
+      <div>
+        <PaypalExpressBtn
+          env={env}
+          client={client}
+          currency={currency}
+          total={total}
+          onError={onError}
+          onSuccess={onSuccess}
+          onCancel={onCancel}
+          style={{
+            size:'large',
+            color: 'blue',
+            shape: 'rect',
+            label: 'checkout'
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default Paypal;
