8499bd276234e1e0820da85f837ce126240f5c0d
ductoanthanh <toanth@metropolia.fi>
2018-08-26 12:10:42 +0300

    Setup actions, reducers for site info

---
+++ client/src/reducers/siteReducer.js
@@ -0,0 +1,15 @@
+// import {
+//
+// } from './types';
+
+export default function(state={}, action) {
+  switch(action.type) {
+    // case CLEAR_UPDATE_USER_DATA:
+    //   return {
+    //     ...state,
+    //     updateUser: action.payload,
+    //   }
+    default:
+      return state;
+  }
+}
