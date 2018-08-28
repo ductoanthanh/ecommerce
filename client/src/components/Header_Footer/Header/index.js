0c4ed7f0d893a88217d83efb7336f54a53018b4a
ductoanthanh <toanth@metropolia.fi>
2018-08-28 23:12:16 +0300

    Update product page layout

--- client/src/components/Header_Footer/Header/index.js
+++ client/src/components/Header_Footer/Header/index.js
@@ -110,7 +110,7 @@ class Header extends Component {
 
   render() {
     return (
-      <header className="bck_b_light">
+      <header className="bck_b_light row">
         <div className="container">
           <div className="left">
             <div className="logo">
