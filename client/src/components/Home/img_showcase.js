de4f9bf74c9554b4b3892c4da63c9fd1f2a9b88a
ductoanthanh <toanth@metropolia.fi>
2018-08-30 18:44:53 +0300

    Add ImgShowcase section

---
+++ client/src/components/Home/img_showcase.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const ImgShowcase = () => {
+  return (
+    <div className="section-showcase">
+            <ul className="images-showcase clearfix">
+                <li>
+                    <figure className="showcase-photo">
+                        <img src="https://res.cloudinary.com/dcdph91rz/image/upload/v1535643694/Screen_Shot_2018-08-30_at_18.41.04.png" alt="Hack Talk 2017" />
+                    </figure>
+                </li>
+                <li>
+                    <figure className="showcase-photo">
+                        <img src="https://images.unsplash.com/photo-1524650359799-842906ca1c06?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=743a655cbb8c62af3cc4af24bb315f11&auto=format&fit=crop&w=1100&q=80" alt="Hack Talk 2017" />
+                    </figure>
+                </li>
+                <li>
+                  <figure className="showcase-photo">
+                      <img src="https://scontent-arn2-1.cdninstagram.com/vp/0f279174107328dbc0babc0d2c9ef843/5BFEE046/t51.2885-15/e35/39401522_238572940182380_7983174112438648832_n.jpg" alt="Hack Talk 2017" />
+                  </figure>
+                </li>
+                <li>
+                  <figure className="showcase-photo">
+                      <img src="https://scontent-arn2-1.cdninstagram.com/vp/032e57de3ccde03bc6df4baead06257b/5C2B279B/t51.2885-15/e35/39397356_531736887288523_2017621802817159168_n.jpg" alt="Hack Talk 2017" />
+                  </figure>
+                </li>
+            </ul>
+            <ul className="images-showcase clearfix">
+                <li>
+                    <figure className="showcase-photo">
+                        <img src="https://res.cloudinary.com/dcdph91rz/image/upload/v1535643522/Screen_Shot_2018-08-30_at_18.33.38.png" alt="Hack Talk 2017" />
+                    </figure>
+                </li>
+                <li>
+                    <figure className="showcase-photo">
+                        <img src="https://res.cloudinary.com/dcdph91rz/image/upload/v1535643522/Screen_Shot_2018-08-30_at_18.34.12.png" alt="Hack Talk 2017" />
+                    </figure>
+                </li>
+                <li>
+                    <figure className="showcase-photo">
+                        <img src="https://res.cloudinary.com/dcdph91rz/image/upload/v1535643522/Screen_Shot_2018-08-30_at_18.32.08.png" alt="Hack Talk 2017" />
+                    </figure>
+                </li>
+                <li>
+                    <figure className="showcase-photo">
+                        <img src="https://res.cloudinary.com/dcdph91rz/image/upload/v1535643521/Screen_Shot_2018-08-30_at_18.33.23.png" alt="Hack Talk 2017" />
+                    </figure>
+                </li>
+            </ul>
+        </div>
+  )
+}
+
+export default ImgShowcase;
