8c3863b589c822bdcdfbd26b37d6985dda1634e5
ductoanthanh <arsenaltoan10@gmail.com>
2018-08-31 12:24:47 +0300

    Update footer mailing list section

--- client/src/components/Header_Footer/Footer/index.js
+++ client/src/components/Header_Footer/Footer/index.js
@@ -9,9 +9,25 @@ const Footer = ({data}) => {
   return (
     data.siteData ?
     <footer className="row">
-      <div className="col-md-12">
+      <div className="home_promotion">
+        <div className="home_promotion_img"
+            style={{
+                background:`url(https://cdn.shopify.com/s/files/1/1678/5005/files/bg-acoutic_1024x1024.jpg?v=1482987733)`
+            }}
+        >
+          <div style={{marginBottom: '80px'}}>
+            <h2 className="tag-title" style={{color: '#fff'}}>Sign Up For Newsletter</h2>
+            <h3 className="tag-low-title" style={{color: '#fff'}}>Sign up for mailing list to get latest updates and offers</h3>
+          </div>
+          <form className="form_newsletter">
+            <input className="form-control" type="email" placeholder="Join mailing list" name="email" id="email" />
+            <button id="subscribe" type="submit">Subscribe</button>
+          </form>
 
-        <div className="wrapper row">
+        </div>
+      </div>
+      <div className="col-md-12">
+        <div className="wrapper row" style={{marginTop: '30px'}}>
           <div className="col-md-2">
             <div className="logo">VIBE</div>
             <p style={{textAlign: 'center'}}>© 2018 Copyright Vibe Inc. All Right Reserved.</p>
@@ -83,7 +99,6 @@ const Footer = ({data}) => {
                 <a href="">Contact Us</a>
               </div>
             </div>
-
           </div>
           <div className="col-md-3">
             <h2>Subscribe</h2>
